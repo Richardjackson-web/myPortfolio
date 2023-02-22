2b76acd61d3bdd1536aae878d84f186d99821955
Richardjackson-web <richardjackson556@gmail>
2023-02-22 22:30:00 +0100

    I worked on the resume...still a part of the about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -44,6 +44,40 @@ function About() {
         <AiFillTwitterCircle className="social-icons" />&nbsp;&nbsp;
         </div>
     </Container>
+
+    <Container>
+      <Row>
+          <div>
+          <hr className="about-line"/>
+          <h2 className="about-head">Resume</h2>
+          </div>
+            <Col sm={7} className="about-experience">
+            <h4 className="about-edu">Experience</h4>
+                <h4 className="about-title">Wedigraf Technologies &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="month">Aug. 2022 - Nov. 2022</span></h4>
+                <h4 className="about-content">Frontend Developer Tutor</h4>
+                <p className="about-content">I taught and mentored beginners on HTML, CSS, JavaScript, Git/GitHub and Reactjs to help them become better developers and build responsive and great looking websites and web applications.</p><br/>
+
+                <h4 className="about-title">Zuri Chat &nbsp;&nbsp;&nbsp;&nbsp;Sep. 2021 - Dec. 2021</h4>
+                <h4 className="about-content">Frontend Developer</h4>
+                <p className="about-content">I lead a team of 100 developers across different continents to build a company holiday calendar plugin to be integrated into Zuri Chat (a slack clone but embedded with great futures).</p><br/>
+
+                <h4 className="about-title">Icevolt &nbsp;&nbsp;&nbsp;&nbsp;Oct. 2021 - Nov. 2021</h4>
+                <h4 className="about-content">Frontend Developer</h4>
+                <p className="about-content">I built a responsive user interface for a web application where one can order solar equipment and tailoring it a specific specifications of need.</p><br/>
+            </Col>
+            <Col sm={5} className="about-education">
+            <h4 className="about-edu">Education</h4>
+                <h4 className="about-title">University of Uyo &nbsp;&nbsp;&nbsp;&nbsp;November 2021</h4>
+                <p className="about-content">B.sc, Biochemistry.</p><br/>
+
+                <h4 className="about-title">Great Faith Comprehensive College &nbsp;&nbsp;&nbsp;&nbsp;July 2013</h4>
+                <p className="about-content">WAEC.</p><br/>
+
+                <h4 className="about-title">James and Jones Nur./Pri. School &nbsp;&nbsp;&nbsp;&nbsp;June 2007</h4>
+                <p className="about-content">FSLC.</p><br/>
+            </Col>
+      </Row>
+    </Container>
     </div>
   )
 }
