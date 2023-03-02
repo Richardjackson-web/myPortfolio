9b7a1e46f1ef7048afc62b13b4039b032474845a
Richardjackson-web <richardjackson556@gmail>
2023-03-02 02:36:06 +0100

    I started out on the contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Portfolio from './Pages/Portfolio/Portfolio';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About/>
       <Portfolio/>
+      <Contact/>
     </div>
   );
 }
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,82 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import {AiFillTwitterCircle} from 'react-icons/ai';
+import {RiWhatsappFill, RiInstagramFill, RiFacebookCircleFill} from 'react-icons/ri';
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <Container className="about-main">
+        <Row className="row-about">
+          <div>
+          <hr className="about-line"/>
+          <h2 className="about-head">Contact me</h2>
+          </div>
+            
+        </Row>
+        <Form className="contact-form">
+      <Row>
+        <Col md={3} xs={0}>
+
+        </Col>
+        <Col md={3} xs={12}>
+          <Form.Control placeholder="First name" />
+        </Col>
+        <br/>
+        <Col md={3} xs={12}>
+          <Form.Control placeholder="Last name" />
+        </Col>
+        <Col md={3} xs={0}>
+          
+        </Col>
+      </Row>
+      <br/>
+      <Row>
+      <Col md={3} xs={0}>
+          
+          </Col>
+        <Col md={6} xs={12}>
+        <Form.Control placeholder="Last name" />
+        </Col>
+        <Col md={3} xs={0}>
+          
+        </Col>
+      </Row>
+      <br/>
+      <Row>
+      <Col md={3} xs={3}>
+          
+        </Col>
+        <Col md={6} xs={12}>
+        <Form.Control
+          as="textarea"
+          placeholder="Leave a comment here"
+          style={{ height: '100px' }}
+        />
+        </Col>
+        <Col md={3} xs={3}>
+          
+          </Col>
+      </Row>
+      <br/>
+      <Row>
+        <Col>
+        <button>Email</button> 
+        </Col>
+      </Row>
+    </Form>
+        <div className="social-holder">
+        <RiWhatsappFill className="social-icons" />&nbsp;&nbsp;
+        <RiInstagramFill className="social-icons" />&nbsp;&nbsp;
+        <RiFacebookCircleFill className="social-icons" />&nbsp;&nbsp;
+        <AiFillTwitterCircle className="social-icons" />&nbsp;&nbsp;
+        </div>
+    </Container>
+    </div>
   )
 }
 
