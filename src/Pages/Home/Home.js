195bce4f5ac4520ee6d40eb2a39f1a7dc1c40678
Richardjackson-web <richardjackson556@gmail.com>
2023-05-23 23:56:26 +0100

    I fixed another bug...just responsiveness issue.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -20,11 +20,11 @@ function Home() {
     <div id="myP">
     <Container>
         <Row className="row-home">
-            <Col sm={5} className="boxOne">
+            <Col sm={6} className="boxOne">
                 <div className="hold">
                     <h1 className="home-text">Hi There!<br/> I'm Richard.</h1>
                     <p className="home-front">Frontend Web Developer.</p>
-                    <Button className="home-button">Learn More</Button>
+                    <Button href="https://my-portfolio-richard.vercel.app/about" className="home-button">Click here to know me...</Button>
                     <div className="icon-holder">
                       <FaHtml5 className="icons" />&nbsp;&nbsp;
                       <FaCss3 className="icons" />&nbsp;&nbsp;
@@ -33,7 +33,7 @@ function Home() {
                     </div>
                 </div>
             </Col>
-            <Col sm={5} className="boxTwo">
+            <Col sm={6} className="boxTwo">
             <div>
 
             </div>
@@ -43,10 +43,10 @@ function Home() {
             </div>
         </Row>
         <div className="home-social-holder">
-        <RiWhatsappFill className="home-social-icons" />&nbsp;&nbsp;
-        <RiInstagramFill className="home-social-icons" />&nbsp;&nbsp;
-        <RiFacebookCircleFill className="home-social-icons" />&nbsp;&nbsp;
-        <AiFillTwitterCircle className="home-social-icons" />&nbsp;&nbsp;
+        <RiWhatsappFill href="https://wa.link/dsyz46" className="home-social-icons" />&nbsp;&nbsp;
+        <RiInstagramFill href="https://www.instagram.com/i_am_richaard/" className="home-social-icons" />&nbsp;&nbsp;
+        <RiFacebookCircleFill href="https://web.facebook.com/Richard.jackson1010" className="home-social-icons" />&nbsp;&nbsp;
+        <AiFillTwitterCircle href="https://twitter.com/i_am_richaard?s=08" className="home-social-icons" />&nbsp;&nbsp;
         </div>
     </Container>
     </div>
